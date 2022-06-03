1cb4652a0e2a36250f5291e9f694a72af5a84bfc
codewithmiguel <codewithmiguel@gmail.com>
2022-06-03 16:16:19 -0300

    🚀feat::  Component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
+import { Header } from './components/Header';
+import { Post } from './Post';
+import styles from './App.module.css';
+import { SideBar } from './components/SideBar';
 
 function App() {
-  
+
   return (
     <>
-      Hello
+      <Header></Header>
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post author="pedro" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+          <Post author="joao" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+          <Post author="edson" content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
+
+        </main>
+      </div>
     </>
   )
 }
