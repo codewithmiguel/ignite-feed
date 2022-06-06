b26f53ea2a2482b4e169e9dbb89aef2ffd0c9430
codewithmiguel <codewithmiguel@gmail.com>
2022-06-06 17:13:08 -0300

    ✔️ Chore: styled commment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,6 +30,20 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentário...'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+
+
     </article>
   )
 }
