8ad51a3956aeef22542a9e7ccabceb713c2f19f3
codewithmiguel <codewithmiguel@gmail.com>
2022-06-14 09:49:49 -0300

    👊 feat:  react useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,10 +3,13 @@ import { Commment } from './Comment';
 import styles from './Post.module.css';
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
 
   // console.log(content)
+
+  const [comments, setComments] = useState([1, 2,]);
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -16,6 +19,13 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+  }
+
 
   return (
     <article className={styles.post}>
@@ -48,7 +58,10 @@ export function Post({ author, publishedAt, content }) {
         }
       </div>
 
-      <form className={styles.commentForm}>
+      <form
+        onSubmit={handleCreateNewComment}
+        className={styles.commentForm}
+      >
         <strong>Deixe seu feedback</strong>
 
         <textarea
@@ -61,10 +74,7 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-
-        <Commment />
-        <Commment />
-        <Commment />
+        {comments.map(item => <Commment />)}
       </div>
 
     </article>
