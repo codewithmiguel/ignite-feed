630d241d2320cc99838a33c49f4459341f2c189f
codewithmiguel <codewithmiguel@gmail.com>
2022-06-15 08:50:09 -0300

    ⚒️ bugfix: added key in lists

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -55,9 +55,9 @@ export function Post({ author, publishedAt, content }) {
         {
           content.map(line => {
             if (line.type === 'paragraph') {
-              return <p>{line.content}</p>;
+              return <p key={line.content}>{line.content}</p>;
             } else if (line.type === 'link') {
-              return <p><a href="">{line.content}</a></p>;
+              return <p key={line.content}><a href="">{line.content}</a></p>;
             }
           })
         }
@@ -82,7 +82,7 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        {comments.map(comment => <Commment content={comment} />)}
+        {comments.map(comment => <Commment key={comment} content={comment} />)}
       </div>
 
     </article>
